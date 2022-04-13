e17b9325b18934a1b8bc6df01eaf1f193c72f468
elliej1994 <102549808+elliej1994@users.noreply.github.com>
2022-04-13 09:55:29 -0400

    working example2

--- src/main/java/dev/johnson/api/BankApp.java
+++ src/main/java/dev/johnson/api/BankApp.java
@@ -22,23 +22,24 @@ public class BankApp {
         Scanner scanner = new Scanner(System.in);
 
 
-        System.out.println("Welcome to the StruggleIndustries(TM) Banking Terminal");
-        System.out.println("1. Create New Account \n2. Forgot username? \n3.Login \n4.Exit");
-        int input = scanner.nextInt();
-        scanner.nextLine();
-
+        int input;
+        do {
+            System.out.println("Welcome to the Ambiguous Corporate Industries(TM) Banking Terminal");
+            System.out.println("1. Create New Account \n2. Forgot username? \n3.Login \n4.Exit");
+            input = scanner.nextInt();
+            scanner.nextLine();
             switch (input) {
                 case 1:
-                    System.out.println("We're happy to welcome you to our family of just good enough products");
-                    System.out.println("Please enter your first name :");
+                    System.out.println("We're happy to welcome you to our contractual family!");
+                    System.out.println("Please enter your first name:");
                     String fName = scanner.nextLine();
-                    System.out.println("Please enter your last name :");
+                    System.out.println("Please enter your last name:");
                     String lName = scanner.nextLine();
-                    System.out.println("Please enter your preffered Phone number :");
+                    System.out.println("Please enter your preferred Phone number:");
                     String mobileNo = scanner.nextLine();
                     System.out.println("Please enter your desired username:");
                     String userName = scanner.nextLine();
-                    System.out.println("Please enter the password you would like to set, please keep a copy for your own records. ");
+                    System.out.println("Please enter your desired password,  please keep a copy for your own records.");
                     String password = scanner.nextLine();
                     BankAccount bankAccount = BankApp.bankAccountService.createBankAccount(0, fName, lName, mobileNo, userName, password);
                     System.out.println("Your new account has been created!" + bankAccount);
@@ -61,35 +62,36 @@ public class BankApp {
                     BankAccount b = BankApp.bankAccountService.login(accountUserName, accPass);
                     System.out.println("Your balance is: $" + b.getBalance() + " .\n Your account number is " + b.getAccountNo());
 
-                    System.out.println("1.Make deposit. \n2.Withdrawal ");
-                    int input2 = scanner.nextInt();
+                    System.out.println("A.Make deposit. \nB.Withdrawal ");
+                    String branch = scanner.nextLine().toUpperCase();
                     scanner.nextLine();
 
-                    switch (input2) {
+                    switch (branch) {
 
-                        case 1:
-                            System.out.println("Enter the amount you'd like to be deposited");
+                        case "A":
+                            System.out.println("Enter the amount you'd like to have deposited:");
                             Double val = scanner.nextDouble();
                             BankAccount a = bankAccountService.makeDeposit(b, val);
-                            System.out.println("Your balance is now: " + a.getBalance() + " Thank you for your continued patronage.");
+                            System.out.println(a.getFirstName()+ " your balance is now: " + a.getBalance() + " Thank you for your continued patronage.");
                             break;
-                        case 2:
-                            System.out.println("Enter the amount you'd like withdrawn");
+                        case "B":
+                            System.out.println("Enter the amount you'd like to have withdrawn:");
                             Double val1 = scanner.nextDouble();
                             BankAccount j = bankAccountService.makeWithdrawal(b, val1);
-                            System.out.println("Your balance is now: " + j.getBalance() + " \nPress 3 to Exit");
-                            break;
+                            if((j.getBalance()-val1)<0) {
+                                System.out.println("Your balance is now: " + (j.getBalance()- 35 )+ " \nPlease be aware there is a $35 charge associated with each overdraft.");
+                            }else System.out.println("Your balance is now:" + j.getBalance());
+                           break;
                         default:
                             System.out.println("The selection was invalid");
                     }
                 case 4:
-                    System.out.println("Exiting...Have a great day!");
-                    System.exit(4);
+                    System.out.println("...exiting...");
                     break;
                 default:
                     System.out.println("The selection was invalid");
-
             }
-
         }
+        while (input !=4 );
     }
+}
